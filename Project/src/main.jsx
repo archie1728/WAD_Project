cf5c8aa3ae08c853346c709622fa1430ca2133cb
archie1728 <archit1379@gmail.com>
2024-08-25 20:59:50 +0700

    Change to index links

--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
             element: <HomePage />,
           },
           {
-            path: "/WAD_Project/Car-Details/",
+            path: "Car-Details",
             element: <CarDetailsPage />,
           },
         ],
