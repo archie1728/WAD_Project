0e024b0849453e27d4a1193018745d410e545ab7
lorenzodlm <145830734+lorenzodlm@users.noreply.github.com>
2024-08-25 21:40:25 +0700

    Update CarDetailsPage.jsx

--- Project/src/route/CarDetailsPage.jsx
+++ Project/src/route/CarDetailsPage.jsx
@@ -216,7 +216,7 @@ const CarDetailsPage = () => {
                                         </Card.Text>
                                         <Button 
                                             variant={highlight.find(c => c.Cid === car.Cid) ? 'danger' : 'primary'} 
-                                            onClick={() => highlightCar(car)}
+                                            onClick={() => highlight.some(c => c.Cid === car.Cid) ? removeHighlight(car) : highlightCar(car)}
                                         >{highlight.find(c => c.Cid === car.Cid) ? 'Remove Highlight' : 'Highlight'}
                                         </Button>
                                     </Card.Body>
